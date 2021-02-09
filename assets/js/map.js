e1bc72fe9a68cfa53e8bf2496a65ec52ad0ef0bf
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-02-09 21:00:15 +0000

    Update map marker to gold pointer to match branding and logo, upload new marker to img folder

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,7 +1,6 @@
 /* Resource: Google Maps JavaScript API Tutorial https://www.youtube.com/watch?v=Zxf1mnP5zcw and Code Institute's Google Maps module */
 
     // Initialise and add map of Ireland
-
     function initMap(){
         // Link to map id in HTML
         let map = new google.maps.Map(document.getElementById("map"), {
@@ -10,30 +9,29 @@
             center: {lat: 53.1424, lng: -7.6921},
         });
 
-        // Add markers
-       // let marker = new google.maps.Marker({
-         //   position: locations,
-           // map: map,
-        //});
-    //}
-
-
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-        var markers = dataset.map(function(location, i){
+    var markers = dataset.map(function(location, i){
+        return new google.maps.Marker({
+            //Pull lat and lng values from dataset.js
+            position: {lat: location.lat, lng: location.lng},
+            label: labels[i % labels.length],
+            map: map,
+            /* -- Resource: Create custom markers - https://developers.google.com/maps/documentation/javascript/custom-markers */ 
+            /* -- Resource: Image made by https://www.freepik.com on https://www.flaticon.com/ */ 
+            icon: 'assets/img/map-pointer.png',
+        });
+        /*var content = dataset.map(function(InfoWindow, i){
             return new google.maps.Marker({
-                position: {lat: location.lat, lng: location.lng},
-                label: labels[i % labels.length],
-                map: map,
-                /* -- Resource: Create custom markers - https://developers.google.com/maps/documentation/javascript/custom-markers */ 
-                /* -- Resource: Image made by https://www.freepik.com on https://www.flaticon.com/ */ 
-                icon: 'assets/img/whiskey-cask.png',
-           });
+                name: {title: InfoWindow.title},
+        });*/
     });
 
-        /* Resource: https://developers.google.com/maps/documentation/javascript/marker-clustering */
-        new MarkerClusterer(map, markers, {
+
+
+    /* Resource: https://developers.google.com/maps/documentation/javascript/marker-clustering */
+    new MarkerClusterer(map, markers, {
             imagePath:
-               "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m"
+            "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m"
         });
     }
