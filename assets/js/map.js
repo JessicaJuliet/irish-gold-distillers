3eb4717bef5e38171cf4e75b9783a95b50bb1648
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-02-10 18:13:42 +0000

    Fix bug which was causing map to not dispaly

--- assets/js/map.js
+++ assets/js/map.js
@@ -9,29 +9,22 @@
             center: {lat: 53.1424, lng: -7.6921},
         });
 
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    var markers = dataset.map(function(location, i){
-        return new google.maps.Marker({
-            //Pull lat and lng values from dataset.js
-            position: {lat: location.lat, lng: location.lng},
-            label: labels[i % labels.length],
-            map: map,
-            /* -- Resource: Create custom markers - https://developers.google.com/maps/documentation/javascript/custom-markers */ 
-            /* -- Resource: Image made by https://www.freepik.com on https://www.flaticon.com/ */ 
-            icon: 'assets/img/map-pointer.png',
-        });
-        /*var content = dataset.map(function(InfoWindow, i){
+        var markers = dataset.map(function(location, i){
             return new google.maps.Marker({
-                name: {title: InfoWindow.title},
-        });*/
-    });
+                //Pull lat and lng values from dataset.js
+                position: {lat: location.lat, lng: location.lng},
+                label: labels[i % labels.length],
+                map: map,
+                /* -- Resource: Create custom markers - https://developers.google.com/maps/documentation/javascript/custom-markers / Image made by https://www.freepik.com on https://www.flaticon.com/ */ 
+                icon: 'assets/img/map-pointer.png',
+            });
+        });
 
-
-
-    /* Resource: https://developers.google.com/maps/documentation/javascript/marker-clustering */
-    new MarkerClusterer(map, markers, {
+        /* Resource: https://developers.google.com/maps/documentation/javascript/marker-clustering */
+        new MarkerClusterer(map, markers, {
             imagePath:
-            "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m"
+            "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
         });
     }
